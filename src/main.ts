5a31395d05f2df1546abaa5bd6bdeecac250c041
gibbs.shih <gibbs.shih@isuncloud.com>
2024-03-08 17:58:27 +0800

    change to test mode

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   const parser = new Parser();
 
   // Promise.all([crawler.start()]);
-  // Promise.all([crawler.start(), parser.start()]);
+  Promise.all([crawler.start(), parser.start()]);
   // Promise.all([parser.start()]);
 }
 
