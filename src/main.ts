2ede788ccf8af4c6f5a60118bd05f47441ec7571
gibbs.shih <gibbs.shih@isuncloud.com>
2024-03-13 11:11:49 +0800

    upadte schema

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import Crawler from "./crawler";
 import Parser from "./parser";
+import ReportAndMint from "./create_report_mint_nft";
 import { config } from "dotenv";
 
 const HTTP_PORT = process.env.HTTP_PORT || 3000;
@@ -16,10 +17,11 @@ async function bootstrap() {
 
   const crawler = new Crawler();
   const parser = new Parser();
+  const reportAndMint = new ReportAndMint();
 
   // Promise.all([crawler.start()]);
   // Promise.all([crawler.start(), parser.start()]);
-  Promise.all([parser.start()]);
+  Promise.all([reportAndMint.start(), parser.start()]);
 }
 
 bootstrap();
