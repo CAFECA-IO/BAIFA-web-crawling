4b3d4a28b016df7c7464ee0a94d2b89b89610999
gibbs.shih <gibbs.shih@isuncloud.com>
2024-03-15 18:42:21 +0800

    update config order

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import Parser from "./parser";
 import ReportAndMint from "./create_report_mint_nft";
 import { config } from "dotenv";
 
-const HTTP_PORT = process.env.HTTP_PORT || 3000;
+
 
 if (process.env.NODE_ENV !== "production") {
   config();
 }
+
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
